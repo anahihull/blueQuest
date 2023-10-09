a1b8802d5f209fe6ca4d93c0cbf4f9f5dcfad9a6
diangelob <beramirez272gmail.com>
2023-10-08 18:00:11 -0700

    intercambie nombres de home y learn

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,186 +1,120 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { RadioButton } from 'react-native-paper';
-import Modal from 'react-native-modal';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Questions = [
-  {
-    "Answers": [
-      "Through their skin",
-      "By drinking water",
-      "Using gills"
-    ],
-    "Correct Answer": "Using gills",
-    "ID": "'1'",
-    "Question": "What is the main way fish breathe underwater?"
-  },
-  {
-    "Answers": [
-      "Dolphins",
-      "Turtles",
-      "Eels"
-    ],
-    "Correct Answer": "Eels",
-    "ID": "'2'",
-    "Question": "Which of these animals lives in both saltwater and freshwater environments?"
-  },
-  {
-    "Answers": [
-      "Seaweeds",
-      "Plankton",
-      "Coral reefs"
-    ],
-    "Correct Answer": "Plankton",
-    "ID": "'3'",
-    "Question": "What is the term for tiny floating organisms in the ocean that form the base of the marine food chain?"
-  },
-  {
-    "Answers": [
-      "Swimming",
-      "Photosynthesis",
-      "Digestion"
-    ],
-    "Correct Answer": "Photosynthesis",
-    "ID": "'4'",
-    "Question": "What process do aquatic plants use to make their own food using sunlight?"
-  },
-  {
-    "Answers": [
-      "Seashells",
-      "Plastic bags",
-      "Algae blooms"
-    ],
-    "Correct Answer": "Algae blooms",
-    "ID": "'5'",
-    "Question": "What poses a danger to aquatic ecosystems by blocking sunlight and reducing oxygen levels in the water?"
-  }
-];
-
-const getRandomQuestion = () => {
-  const randomNum = Math.floor(Math.random() * Questions.length);
-  return Questions[randomNum];
-};
-
-const LearnScreen = () => {
-  const [question, setQuestion] = useState(getRandomQuestion());
-  const [selectedAnswer, setSelectedAnswer] = useState('');
-  const [showResult, setShowResult] = useState(false);
-  const [isModalVisible, setModalVisible] = useState(false);
-
-  const handleAnswerChange = (answer) => {
-    setSelectedAnswer(answer);
-    setShowResult(false);
+const HomeScreen = ({ navigation }) => {
+  const navigateToAquaticGame = () => {
+    navigation.navigate('CleanOcean');
   };
 
-  const checkAnswer = () => {
-    setShowResult(true);
+  const navigateToWaterConservationGame = () => {
+    navigation.navigate('Faucet');
+  };
 
-    if (isCorrectAnswer()) {
-      setModalVisible(true);
+  const MyCard = ({ title, content }) => {
+    return (
+      <Card>
+        <Card.Title>{title}</Card.Title>
+        <Card.Divider />
+        <Text style={{ marginBottom: 10 }}>
+          {content}
+        </Text>
+      </Card>
+    );
+  };
+
+  const data = [
+    {
+      title: 'Lesson 1: Seas of Plastic',
+      content: ' Oceanic waters are being inundated with millions of tons of plastic waste annually, posing a threat to marine life and ecosystems. Adopting responsible consumption habits and enhancing waste management systems is imperative to shield our seas.'
+    },
+    {
+      title: 'Lesson 2: Ocean Acidification',
+      content: 'The ocean absorption of CO2 is altering its pH, negatively impacting marine organisms, especially those reliant on calcification like corals and mollusks. It is vital to mitigate greenhouse gas emissions to safeguard marine biodiversity.'
+    },
+    {
+      title: 'Lesson 3: The Significance of Marine Protected Areas',
+      content: 'Establishing and maintaining Marine Protected Areas (MPAs) is vital for preserving marine ecosystems and the species that inhabit them. These zones serve as safe havens for biodiversity and help sustain fish populations, benefiting fishing communities as well.'
+    },
+    {
+      title: 'Lesson 4: Overflow of Contaminants into the Ocean',
+      content: 'Oil spills, agricultural fertilizer runoff, and other pollutants regularly enter the ocean, causing long-term damage to marine fauna and flora. Implementing cleaner industrial and agricultural practices is vital to curtail ocean pollution.'
+    },
+    {
+      title: 'Lesson 5: Overfishing: An Ocean on the Brink of Depletion',
+      content: 'Overexploitation of fishing resources threatens the sustainability of oceanic ecosystems and the food security of millions of people. Adopting sustainable fishing practices and strict regulations is essential for preserving marine species.'
+    },
+    {
+      title: 'Lesson 6: The Crucial Role of Mangroves',
+      content: 'Mangroves play a pivotal role in protecting coastlines, nurturing fish populations, and sequestering carbon, providing vital services to both marine ecosystems and human communities. Protecting and rehabilitating mangrove forests is key to maintaining these benefits and supporting healthy oceans.'
     }
-  };
+  ];
 
-  const isCorrectAnswer = () => {
-    return selectedAnswer === question['Correct Answer'];
-  };
-
-  const handleNextQuestion = () => {
-    setQuestion(getRandomQuestion());
-    setSelectedAnswer('');
-    setShowResult(false);
+  const CardList = ({ data }) => {
+    return (
+      <View style={{ backgroundColor: 'transparent' }}>
+        {data.map((item, index) => (
+          <MyCard
+            key={index}
+            title={item.title}
+            content={item.content}
+          />
+        ))}
+      </View>
+    );
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Trivia Time!</Text>
-      <Text style={styles.question}>{question.Question}</Text>
-
-      {question.Answers.map((answer, index) => (
-        <View key={index} style={styles.optionContainer}>
-          <RadioButton
-            value={answer}
-            status={selectedAnswer === answer ? 'checked' : 'unchecked'}
-            onPress={() => handleAnswerChange(answer)}
-            disabled={showResult}
-          />
-          <Text style={styles.optionText}>{answer}</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.subtitleContainer}>
+          <Text style={styles.subtitleText}>Minigames:</Text>
         </View>
-      ))}
-
-      <Button
-        title="Verify Answer"
-        onPress={checkAnswer}
-        disabled={!selectedAnswer || showResult}
-      />
-
-      {showResult && (
-        <Text style={isCorrectAnswer() ? styles.correctText : styles.incorrectText}>
-          {isCorrectAnswer() ? '¡Correct answer!' : 'Incorrect answer. Try again.'}
-        </Text>
-      )}
-
-      {showResult && !isCorrectAnswer() && (
-        <Button
-          title="Try again"
-          onPress={handleNextQuestion}
-        />
-      )}
-
-      <Modal isVisible={isModalVisible}>
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalText}>Congratulations!</Text>
-          <Text style={styles.modalText}>You unlocked a new lesson.</Text>
-          <Button title="Close" onPress={() => setModalVisible(false)} />
+        <TouchableOpacity style={styles.roundButton} onPress={navigateToAquaticGame}>
+          <Text style={styles.buttonText}>Start Aquatic Game</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.roundButton} onPress={navigateToWaterConservationGame}>
+          <Text style={styles.buttonText}>Start Water Conservation Game</Text>
+        </TouchableOpacity>
+        <View style={styles.remindersContainer}>
+          <CardList data={data} />
         </View>
-      </Modal>
-    </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  question: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  optionContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  optionText: {
-    flex: 1,
-    fontSize: 16,
-  },
-  correctText: {
-    color: 'green',
-    fontSize: 16,
-    marginTop: 10,
-  },
-  incorrectText: {
-    color: 'red',
-    fontSize: 16,
-    marginTop: 10,
-  },
-  modalContainer: {
-    backgroundColor: 'white',
+    backgroundColor: '#f0f0f0',
     padding: 20,
-    borderRadius: 10,
-    alignItems: 'center',
   },
-  modalText: {
+  subtitleContainer: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  subtitleText: {
     fontSize: 18,
-    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  roundButton: {
+    backgroundColor: '#3498db',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25, // Adjust the value to control the roundness
+    marginBottom: 15,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  remindersContainer: {
+    marginTop: 20,
+    padding: 10,
   },
 });
 
-export default LearnScreen;
+export default HomeScreen;
